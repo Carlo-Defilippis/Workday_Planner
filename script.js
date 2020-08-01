f0e9a27b1b7f44c3bec34d50f119b90cb93a23ab
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2020-08-01 19:44:11 -0400

    input text working

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
 });
 
+
+// Display today's date & time in jumbotron
+$("#date").text("Today is " + (moment().format('dddd, MMMM Do YYYY')));
+$("#current-time").text((moment().format('h:mm:ss a')));
+var update = setInterval( function(){
+    date = moment(new Date());
+    $("#current-time").text((moment().format('h:mm:ss a')));
+}, 1000)
+  setInterval(update, 1000);
+
+
+//Today's Date:
+
+  $("#currentDay").text(moment().format('MMMM Do YYYY'));
+
+
+// console.log(moment().format('h a'))
 var taskList = [];
 // var obj = {"test","test2"}
 
@@ -34,15 +51,16 @@ var taskList = [];
     columnTwo.attr("value", [hour]);
     columnTwo.addClass("text-area");
     columnTwo.attr("id", "text-area")
-    columnTwo.html("Save Here")
+    columnTwo.attr("placeholder", "Enter your task here.")
+    columnTwo.attr("input", "Enter your task here.")
     // creates button for save and third col
     
     var columnThree = $("<div>");
     columnThree.addClass("col-sm-1");
-    columnThree.addClass("btn btn-primary btn-outline-success rounded mt-1 mb-1 text-white");
+    columnThree.addClass("btn btn-primary savedBtn btn-outline-success rounded mt-1 mb-1 text-white text-center");
     columnThree.attr("value", [hour]);
     columnThree.attr("role", "button")
-    columnThree.text("Save Here")
+    columnThree.text("Save Button")
   
     plannerRow.append(columnOne);
     plannerRow.append(columnTwo);
@@ -54,36 +72,36 @@ var taskList = [];
     // plannerRow.text("Test Me");
   }
 
-// Display today's date & time in jumbotron
-$("#date").text("Today is " + (moment().format('dddd, MMMM Do YYYY')));
-$("#current-time").text((moment().format('h:mm:ss a')));
-var update = setInterval( function(){
-    date = moment(new Date());
-    $("#current-time").text((moment().format('h:mm:ss a')));
-}, 1000)
-  setInterval(update, 1000);
-
-
-//Today's Date:
-
-  $("#currentDay").text(moment().format('MMMM Do YYYY'));
-
 //Create 9 rows
 
+var textAreaBox = [];
+
 $(".btn").on("click", function(event) {
+  event.preventDefault();
   var saveButton = $(this).attr("value")
-  var textArea = $(this).attr("value")
-  console.log(typeof textArea)
-  if (saveButton === textArea && textArea.length !== 0) {
-  console.log("Text area Value: ",textArea)
-  console.log("Save Button Value: ",saveButton)
-  getText()
-  }
+  var textData = $( "textarea[value="+ saveButton +"]" ).val()
+
+
+  // getText()
+  console.log("This is the textarea data ",textData)
+//   var comment = $.trim($("#text-area").val(saveButton));
+//             if(comment != ""){
+//                 // Show alert dialog if value is not blank
+//                 alert(comment);
+//             }
+//   console.log(textData)
+//   console.log("Text area Value: ",textArea)
+//   console.log("Save Button Value: ",saveButton)
+//   console.log(typeof parseInt(textAreaBox), typeof parseInt(saveButton), typeof textArea)
+//   if (parseInt(saveButton) === parseInt(textAreaBox) && textArea.length !== 0) {
+//   textAreaBox = [];
+// }
 })
 
-function getText() {
+function getText(comp) {
   var textInfo = $("#text-area").val()
   // arr.map(function(o,i) { Object.assign(o,{id:i+1})})
   // console.log(obj)
-  console.log(textInfo)
+  // console.log("This is the info pulled for the text box: ",textInfo)
+
 }
